8896122bd76233016eccebc480e4406bef0f8b95
icanq <icanq@hotmail.com>
2022-06-10 13:55:59 +0000

    feat: update auth methods

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const express = require("express");
 const routes = require("./routes");
 const cors = require('cors')
 const path = require('path')
+const cookieParser = require('cookie-parser')
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
+
 app.use(cors())
+app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '/client/build')))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -2,18 +2,23 @@ const { User } = require("../models");
 const { verifyToken } = require("../helpers/tokenHandler");
 
 const extractBearerToken = (token) => {
+  // Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NTQ4Njc5MTN9.tUKPKjD9jMZgPvn_j0GSbx1uU8IVSJUHguXtJjU_0RI
   return token.substring(7, token.length)
 }
 
 const authentication = async (req, res, next) => {
   try {
-    const { authorization } = req.headers;
-    console.log(authorization, 'ini auth')
-    if (authorization.startsWith("Bearer ")) {
-      access_token = extractBearerToken(authorization)
-    } else {
-      return;
-    }
+    // if using bearer token as authentication methods
+    // const { authorization } = req.headers;
+    // if (authorization.startsWith("Bearer ")) {
+    //   access_token = extractBearerToken(authorization)
+    // } else {
+    //   return;
+    // }
+
+    // if using cookies as authentication methods
+    const { access_token } = req.cookies;
+
     if (access_token) {
       const decoded = verifyToken(access_token);
       const findUser = await User.findOne({
